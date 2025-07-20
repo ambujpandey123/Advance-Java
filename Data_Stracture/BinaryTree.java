0bd5b98dc2949b859fd5df3c5fbb0101383a4e54
Ambuj pandey <128911576+ambujpandey123@users.noreply.github.com>
2025-07-20 12:51:48 +0530

    Create BinaryTree.java

---
+++ Data_Stracture/BinaryTree.java
@@ -0,0 +1,43 @@
+package data_stracture;
+public class BinaryTree {
+    static class Node{
+		int data;
+		Node left;
+		Node right;
+		Node(int data){
+			this.data=data;
+			this.left=null;
+			this.right=null;
+	}
+	
+}
+    static class binaarytree {
+    	public static int idx=-1;
+		 public static Node buildTree(int nodes[]) {
+			 if(nodes[++idx] == -1) {
+				 return null;
+			 }
+			 Node newNode = new Node(nodes[idx]);
+			 newNode.left = buildTree(nodes);
+			 newNode.right = buildTree(nodes);
+			return newNode;
+		 }
+	}
+	public static void main(String[] args) {
+		 int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1};
+		binaarytree tree = new binaarytree();
+		Node root = tree.buildTree(nodes);
+		System.out.println("Root Node: "+ root.data);
+		System.out.println("Left And Right Child Node: "+ root.left.data + "  "+root.right.data);
+		
+
+	}
+
+}
+
+// Its a PreOrder Traversal The tree Looks Like This :
+//       1
+//      / \
+//     2   3
+//    / \  / \
+//   4   5 6  7
